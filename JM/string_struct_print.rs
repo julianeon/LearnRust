b072657e57df34cd832fa027e5d4f4c5e12ea4e0
julianeon <julianeon@gmail.com>
2020-08-04 11:24:48 -0700

    now w string

--- JM/string_struct_print.rs
+++ JM/string_struct_print.rs
@@ -3,13 +3,21 @@ struct Unit {
     b: String
 }
 
+impl Unit {
+  fn new(a: String, b: String) -> Self {
+    Self { a, b }
+  }
+}
+
 impl std::fmt::Display for Unit {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         write!(f, "(value a: {}, value b: {})", self.a, self.b)
     }
 }
 
+
 fn main() {
-    let test = Unit { a: String::from("joe"), b: String::from("mark") };
-    println!("{}", test);    
+    let uniter=Unit::new("mark".to_string(),"lowe".to_string());
+    println!("{}", uniter);    
+    
 }
