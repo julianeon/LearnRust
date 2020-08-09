d114d33ff7a73d1298dca78aa859edf0bf0af5e8
julianeon <julianeon@gmail.com>
2020-08-09 14:16:57 -0700

    now w new file

---
+++ JM/read_from_file_print_out_lines.rs
@@ -0,0 +1,26 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+fn get_text(filename: String) {
+    // File hosts must exist in current path before this produces output
+    if let Ok(lines) = read_lines(filename) {
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines {
+            if let Ok(ip) = line {
+                println!("{}", ip);
+            }
+        }
+    }
+}
+
+fn main() {
+    let filename="puzzle2.txt";
+    get_text(filename.to_string());
+}
